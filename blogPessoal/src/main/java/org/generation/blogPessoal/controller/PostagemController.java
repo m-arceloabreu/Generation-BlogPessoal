524cab6785e5dcb87cd7e01d3c24b6999f2626b6
Marcelo Abreu <marceloabreu1997@gmail.com>
2022-02-17 13:00:58 -0300

    Primeiro Programa Spring

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,31 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.PostagemRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagns")	//vai aceita coisas de postagem
+@CrossOrigin("*")		//aceita coisa de outras origens como react e afins
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	
+	
+}
