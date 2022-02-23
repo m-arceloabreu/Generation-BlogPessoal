8dfdd363c9155e1744eae79af8d6c68d48b1256c
Marcelo <marceloabreu1997@gmail.com>
2022-02-22 21:01:42 -0300

    Doc JPA

--- blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
@@ -3,6 +3,8 @@ package org.generation.blogPessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BlogPessoalApplication {
 
--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -2,10 +2,20 @@ package org.generation.blogPessoal.repository;
 
 import java.util.List;
 
+
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**	 Classes Repositorys são as classes que farão a persistencia de dados da nossa aplicação
+ *	Os repositorios também farão parte da camada de negocios da nossa aplicação, fornecendo objetos a outras camadas como as de controle ou visão
+
+ **/
+
+/** Repositoy  é a anotação spring que indica que a class é um repositoio.
+ * JPA é uma api que padroniza um conjunto de classes e metodos que devem ser implementados
+ *
+ **/
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
