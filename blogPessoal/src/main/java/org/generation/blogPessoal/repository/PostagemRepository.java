f0d4b180f43d7a2dc68ac31dd5236b83986549fb
Marcelo <marceloabreu1997@gmail.com>
2022-03-04 18:14:00 -0300

    User autentication

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -19,7 +19,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
-		
-		
-	
+
 }
